a55a62bd6a52964dc60edc460282167d25b2fe6f
BAVITH SHETTY <149647959+bavithGVshetty@users.noreply.github.com>
2026-03-05 23:17:24 +0530

    Update App.jsx

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,66 +1,44 @@
-import { useState } from "react";
+import { useState } from "react"
 import axios from "axios";
-import "./App.css";
+import "./App.css"
+function App(){
 
-function App() {
+    const[text,setText]=useState("");
+  const[video,setvideos]=useState([]);
 
-  const [text, setText] = useState("");
-  const [video, setVideos] = useState([]);
-  const [loading, setLoading] = useState(false);
+const getVideos=async()=>{
 
-  const getVideos = async () => {
-
-    try {
-      setLoading(true);
-
-      const res = await axios.post(
-        "https://mood-based-youtube-recommend-3.onrender.com/mood",
-        { text: text }
-      );
-
-      setVideos(res.data);
-    } catch (err) {
-      console.log(err);
-      alert("Error fetching videos");
-    }
-
-    setLoading(false);
-  };
-
-  return (
-    <div className="app">
-
-      <h1>🎵 Mood Based YouTube Recommender</h1>
-
-      <div className="search-box">
-
-        <input
-          placeholder="How is your mood today?"
-          onChange={(e) => setText(e.target.value)}
-        />
-
-        <button onClick={getVideos}>
-          Predict Mood
-        </button>
-
-      </div>
-
-      {loading && <div className="loader"></div>}
-
-      <div className="videos">
-
-        {video.map((v) => (
-          <iframe
-            key={v.id.videoId}
-            src={`https://www.youtube.com/embed/${v.id.videoId}`}
-            title="video"
-          />
-        ))}
-
-      </div>
-
-    </div>
+  const res=await axios.post(
+    "https://mood-based-youtube-recommend-3.onrender.com/mood",
+    {text:text}
   );
+setvideos(res.data);
+}
+
+return (
+  <div className="app">
+    <h1>Mood Deployed Based Recommender</h1>
+
+    <input placeholder="How Is your mood ?" 
+    onChange={(e)=>setText(e.target.value)}/>
+
+    <button onClick={getVideos}>
+      Predict
+    </button>
+
+    <div className="videos">
+{ video.map(v=>(
+      <iframe width="640" 
+      key={v.id.videoId}
+      height="360" 
+      src={`https://www.youtube.com/embed/${v.id.videoId}`} 
+      title="video"/>
+))}
+      </div>
+
+  </div>
+)
 }
 
 export default App;
+Desgin superr Css for this
