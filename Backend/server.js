e03ff315864d398a6a8dca9ab216adeca0814fcb
BAVITH SHETTY <149647959+bavithGVshetty@users.noreply.github.com>
2026-03-05 23:06:30 +0530

    Set default PORT to 5000 if not specified

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,7 @@ const cors=require("cors");
 const moodRoutes=require("./routes/mood");
 const app=express();
 require('dotenv').config();
-const PORT=process.env.PORT
-
+const PORT=process.env.PORT||5000
 app.use(cors());
 app.use(express.json());
 
